edd31ae0cc915c18b524bb2b82351cf60664c095
wwywmy <378297869@qq.com>
2020-07-31 17:33:51 +0800

    增加导出flyway格式

--- src/main/java/com/abc/cmb/app/MySQLAPP.java
+++ src/main/java/com/abc/cmb/app/MySQLAPP.java
@@ -2,6 +2,8 @@ package com.abc.cmb.app;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
 
@@ -10,10 +12,17 @@ public class MySQLAPP {
 	private final static String dirPath = "d:/db/bwzb";
 	private final static String fileFullPath = dirPath + "/" + "bwzb.sql";
 
+	// V2.0.202004262020__sql_example.sql
+	private final static String flywayBaseVersion = "2.0.1000";
+	private final static String[] vs = flywayBaseVersion.split("\\.");
+
 	public static void main(String[] args) {
 
 		String dirPathTable = dirPath + "/" + "table";
 		String dirPathScript = dirPath + "/" + "script";
+		String dirPathFlyway = dirPath + "/" + "flyway";
+		
+		int third = Integer.parseInt(vs[vs.length - 1]);
 
 		StringBuffer buffer = new StringBuffer();
 		int begin = 0;
@@ -61,6 +70,22 @@ public class MySQLAPP {
 						filePath = dirPathScript + "/" + tableName + ".sql";
 					}
 					FileWriter writer = new FileWriter(filePath);
+					writer.write(buffer.toString());
+
+					// flyway
+					
+					third++;
+					// V2.0.202004262020__sql_example.sql
+					String flywayName = StringUtils.join(//
+							"V", //
+							vs[0], ".", vs[1], ".", third, //
+							"__", //
+							tableName, "_", (isTable ? "table" : "script"), //
+							".", "sql"//
+					);
+					
+					filePath = dirPathFlyway + "/" + flywayName;
+					writer = new FileWriter(filePath);
 					writer.write(buffer.toString());
 
 					begin = 0;
